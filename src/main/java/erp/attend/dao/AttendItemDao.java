20c59df5598f19664b213a58326d1fe5e90ce6a8
wnsgml8820-web <wnsgml8820@gmail.com>
2026-08-09 21:17:00 +0900

    AttendItemDao 생성 및 기록 입력 서비스, 리퀘스트 객체 작성
    attend.dao.AttendItemDao 생성(작성중)
    attend.service.AttendRecordRequest 작성
    attend.service.InsertEmpAttendRecordService 작성

---
+++ src/main/java/erp/attend/dao/AttendItemDao.java
@@ -0,0 +1,45 @@
+package erp.attend.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import erp.attend.model.AttendItem;
+
+public class AttendItemDao {
+
+	private static AttendItemDao attendItemDao = new AttendItemDao();
+	public static AttendItemDao getInstance() {
+		return attendItemDao;
+	}
+	private AttendItemDao() {
+	}
+	public AttendItem selectById(Connection conn, int attendItemId) throws SQLException{
+		String sql = "SELECT * FROM ATTEND_ITEM WHERE ATEEND_ITEM_ID=?";
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setInt(1, attendItemId);
+			try(ResultSet rs = pstmt.executeQuery()){
+				if (rs.next()) {
+					return convertAttendItem(rs);
+				}
+			}
+		}
+		return null;
+	}
+	
+    private AttendItem convertAttendItem(ResultSet rs) throws SQLException {
+        AttendItem item = new AttendItem();
+        item.setAttendItemId(rs.getInt("ATTEND_ITEM_ID"));
+        item.setAttendName(rs.getString("ATTEND_NAME"));
+        item.setUnitType(rs.getString("UNIT_TYPE"));
+        item.setAttendGroupId(rs.getInt("ATTEND_GROUP_ID"));
+
+        int deductLeaveId = rs.getInt("DEDUCT_LEAVE_ID");
+        item.setDeductLeaveId(rs.wasNull() ? null : deductLeaveId);   // NULL 가능 컬럼이므로 wasNull() 체크
+
+        item.setWorkHourType(rs.getString("WORK_HOUR_TYPE"));
+        item.setUseYn(rs.getString("USE_YN"));
+        return item;
+    }
+}
