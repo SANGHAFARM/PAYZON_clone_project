5d97e497cfd05db3fd6faceeffd22e611b766725
wnsgml8820-web <wnsgml8820@gmail.com>
2026-08-09 15:08:57 +0900

    attend.dao 작성 및 기타사항 추가
    *attend.dao.EmpAttendRecordDAO작성
    *attend.dao.EmpLeaveDAO 작성
    *atttend.dao.LeaveItemDAO 작성
    *attend.model.AttendDetailItem 작성(근태 상세 조회 결과를 담을 모델)
    *attendmodel.AttendSearchCondition 작성(근태 상세 조회 검색에 사용할 모델)
    *attend.model.EmpLeaveStatusItem 작성(휴가조회 검색결과를 담을 모델)
    *attend.model.LeaveStatusCondition 작성(휴가조회 검색 조건을 담을 모델)
    *hr.dao.EmployeeDAO에 근태 조회에 사용할 조회 메서드 추가
    *hr.model.EmployeeListItem 작성(근태 조회 결과를 담을 메서드)

---
+++ src/main/java/erp/attend/dao/LeaveItemDAO.java
@@ -0,0 +1,52 @@
+package erp.attend.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import erp.attend.model.LeaveItem;
+import jdbc.JdbcUtil;
+
+public class LeaveItemDAO {
+	private static LeaveItemDAO leaveItemDAO = new LeaveItemDAO();
+
+	public LeaveItemDAO getInstance() {
+		return leaveItemDAO;
+	}
+
+	private LeaveItemDAO() {
+
+	}
+
+	public LeaveItem selectUsableByCategory(Connection conn, String category) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM LEAVE_ITEM WHERE ITEM_NAME LIKE ? AND USE_YN='Y'";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "%" + category);
+			rs = pstmt.executeQuery();
+			LeaveItem li = null;
+			if (rs.next()) {
+				li = convertLeaveItem(rs);
+			}
+			return li;
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+
+	private LeaveItem convertLeaveItem(ResultSet rs) throws SQLException {
+		LeaveItem li = new LeaveItem();
+		li.setLeaveItemId(rs.getInt("LEAVE_ITEM_ID"));
+		li.setItemName(rs.getString("ITEM_NAME"));
+		li.setApplyStartDate(rs.getDate("APPLY_START_DATE"));
+		li.setApplyEndDate(rs.getDate("APPLY_END_DATE"));
+		li.setUseYn(rs.getString("USE_YN"));
+		return li;
+	}
+
+}
