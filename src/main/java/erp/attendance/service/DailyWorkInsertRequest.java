538ece326d50b62284ec8abe4f7ca71de7024b24
wnsgml8820-web <wnsgml8820@gmail.com>
2026-08-14 18:00:18 +0900

    일용직 근무기록 서비스 작성 및 dto, dao 수정
    //일용직 근무기록 관리 입력 관련
    DayWorkerManagermentHandler 생성
    DailyWorkInsertService 생성
    DailyWorkInsertRequest 생성
    
    //일용직 근무기록 관리 뷰 관련
    ListDailyWorkRecordsService 생성
    EmployeeDao 일용직 관련 메서드 작성
    DayWorkerDto 작성(일용직 사원 모음)

--- src/main/java/erp/attendance/service/DailyWorkInsertRequest.java
+++ src/main/java/erp/attendance/service/DailyWorkInsertRequest.java
@@ -1,20 +1,27 @@
 package erp.attendance.service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+//일용직 근무기록 입력 리퀘스트
 public class DailyWorkInsertRequest {
+	private List<Integer> employeeIds;
 	private Date workDate;
-	private int projectId;
+	private Integer projectId;
 	private long dailyPay;
 	private double payRate;
 	private long incomeTax;
 	private long localIncomeTax;
 	private long actualPay;
+
 	public DailyWorkInsertRequest() {
 	}
-	public DailyWorkInsertRequest(Date workDate, int projectId, long dailyPay, double payRate, long incomeTax,
-			long localIncomeTax, long actualPay) {
+
+	public DailyWorkInsertRequest(List<Integer> employeeIds, Date workDate, Integer projectId, long dailyPay,
+			double payRate, long incomeTax, long localIncomeTax, long actualPay) {
 		super();
+		this.employeeIds = employeeIds;
 		this.workDate = workDate;
 		this.projectId = projectId;
 		this.dailyPay = dailyPay;
@@ -23,47 +30,82 @@ public class DailyWorkInsertRequest {
 		this.localIncomeTax = localIncomeTax;
 		this.actualPay = actualPay;
 	}
+
+	public List<Integer> getEmployeeIds() {
+		return employeeIds;
+	}
+
+	public void setEmployeeIds(List<Integer> employeeIds) {
+		this.employeeIds = employeeIds;
+	}
+
 	public Date getWorkDate() {
 		return workDate;
 	}
+
 	public void setWorkDate(Date workDate) {
 		this.workDate = workDate;
 	}
-	public int getProjectId() {
+
+	public Integer getProjectId() {
 		return projectId;
 	}
-	public void setProjectId(int projectId) {
+
+	public void setProjectId(Integer projectId) {
 		this.projectId = projectId;
 	}
+
 	public long getDailyPay() {
 		return dailyPay;
 	}
+
 	public void setDailyPay(long dailyPay) {
 		this.dailyPay = dailyPay;
 	}
+
 	public double getPayRate() {
 		return payRate;
 	}
+
 	public void setPayRate(double payRate) {
 		this.payRate = payRate;
 	}
+
 	public long getIncomeTax() {
 		return incomeTax;
 	}
+
 	public void setIncomeTax(long incomeTax) {
 		this.incomeTax = incomeTax;
 	}
+
 	public long getLocalIncomeTax() {
 		return localIncomeTax;
 	}
+
 	public void setLocalIncomeTax(long localIncomeTax) {
 		this.localIncomeTax = localIncomeTax;
 	}
+
 	public long getActualPay() {
 		return actualPay;
 	}
+
 	public void setActualPay(long actualPay) {
 		this.actualPay = actualPay;
 	}
-	
+
+	public void validate(Map<String, Boolean> errors) {
+		if (employeeIds == null || employeeIds.size() == 0) {
+			errors.put("employeeIds", Boolean.TRUE);
+		}
+		if (projectId == null || projectId == 0) {
+			errors.put("projectId", Boolean.TRUE);
+		}
+		if (workDate == null) {
+			errors.put("workDate", Boolean.TRUE);
+		}
+
+	}
+
 }
--- src/main/java/erp/attendance/service/ListDailyWorkRecordService.java
+++ src/main/java/erp/attendance/service/ListDailyWorkRecordService.java
@@ -2,20 +2,24 @@ package erp.attendance.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import erp.attendance.dao.DailyWorkRecordDao;
 import erp.attendance.dto.DailyWorkRecordDto;
 import jdbc.connection.ConnectionProvider;
 
+
+//일용직 개별 사원의 연월별 근무기록을 조회하는 서비스
 public class ListDailyWorkRecordService {
-	
 	private DailyWorkRecordDao dailyWorkRecordDao = DailyWorkRecordDao.getInstance();
-	public DailyWorkRecordDto getDailyWorkRecordDto() {
+	public List<DailyWorkRecordDto> getDailyWorkRecordDto(DailyWorkRecordRequest req) {
+		List<DailyWorkRecordDto> list = null;
 		try(Connection conn = ConnectionProvider.getConnection()){
-			
+			list = dailyWorkRecordDao.selectByRequest(conn, req);
+			return list;
 		} catch (SQLException e) {
+			throw new RuntimeException();
 		}
-		return null;
 	}
 	
 }
