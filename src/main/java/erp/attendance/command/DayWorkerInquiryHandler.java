b52c290ae85cf89bce0c003f732fe7b68ac38a62
wnsgml8820-web <wnsgml8820@gmail.com>
2026-08-17 23:40:59 +0900

    일용직 상세 근무 조회 뷰 수정 및 핸들러 수정
    검색 설정에서 시작일이 종료일보다 늦을 시 오류 메시지 출력하게 수정
    성명, 현장/프로젝트 설정값이 검색 후에도 유지되도록 수정

--- src/main/java/erp/attendance/command/DayWorkerInquiryHandler.java
+++ src/main/java/erp/attendance/command/DayWorkerInquiryHandler.java
@@ -1,8 +1,8 @@
 package erp.attendance.command;
 
-import java.sql.Connection;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -65,36 +65,52 @@ public class DayWorkerInquiryHandler implements CommandHandler{
 			req.setAttribute("jobPositions", jobPositionListService.list());
 		} else if ("detail".equals(view)) {
 			req.setAttribute("view", view);
+			
+
+
+			//현장/프로젝트 목록
+			ProjectDao projectDao = ProjectDao.getInstance();
+			List<Project> projects = projectDao.selectAll(ConnectionProvider.getConnection());
+			req.setAttribute("projects", projects);
+			
+			//파라미터 기반으로 리퀘스트 생성
 			String startStr = req.getParameter("startDate");
 			String endStr = req.getParameter("endDate");
 			String empNameKr = req.getParameter("empNameKr");
 			String departmentIdStr = req.getParameter("departmentId");
 			String projectIdStr = req.getParameter("projectId");
 			DailyWorkDetailRequest request = new DailyWorkDetailRequest();
-			if (startStr!=null && !startStr.trim().isEmpty()&&endStr!=null && !endStr.trim().isEmpty()) {
-				try {
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");	
-					request.setStartDate(sdf.parse(startStr));
-					request.setEndDate(sdf.parse(endStr));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			
 			request.setEmpNameKr(empNameKr);
 			Integer departmentId = (departmentIdStr!=null && !departmentIdStr.isEmpty())?Integer.parseInt(departmentIdStr):null;
 		    Integer projectId = (projectIdStr!=null && !projectIdStr.isEmpty())?Integer.parseInt(projectIdStr):null;
 		    request.setDepartmentId(departmentId);
 		    request.setProjectId(projectId);
-			req.setAttribute("departmentId", departmentId);
-			req.setAttribute("projectId", projectId);
+			if (startStr!=null && !startStr.trim().isEmpty()&&endStr!=null && !endStr.trim().isEmpty()) {
+				try {
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");	
+					Date startDate = sdf.parse(startStr);
+					Date endDate = sdf.parse(endStr);
+					if (endDate.before(startDate)) {
+						startDate=null;
+						endDate=null;
+						req.setAttribute("dateError", "dateError");
+					}
+					request.setStartDate(startDate);
+					request.setEndDate(endDate);
+					req.setAttribute("startDate", startDate!=null?startStr:null);
+					req.setAttribute("endDate", endDate!=null?endStr:null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			
+			//일용직 근무 기록 상세 조회
 		    DailyWorkDetailService dailyWorkDetailService = new DailyWorkDetailService();
 			List<DailyWorkDetailDto> dailyWorkDetail = dailyWorkDetailService.search(request);
 			req.setAttribute("dailyWorkDetail", dailyWorkDetail);
-			ProjectDao projectDao = ProjectDao.getInstance();
-			List<Project> projects = projectDao.selectAll(ConnectionProvider.getConnection());
-			req.setAttribute("projects", projects);
 			
+
 
 		}
 		
